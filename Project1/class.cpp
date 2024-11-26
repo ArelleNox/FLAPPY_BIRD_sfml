bad022d36a8a48d0c76c88f3ad7ecc148024a59c
ArelleNox <janelleroxane@gmail.com>
2024-11-26 17:08:12 +0100

    bird

--- Project1/class.cpp
+++ Project1/class.cpp
@@ -6,6 +6,31 @@
 using namespace sf;
 using namespace std;
 
+class Bird {
+private:
+    
+        sf::Texture birdTexture;
+        if (!birdTexture.loadFromFile("flbird2.png")) {
+            return -1; // Erreur si le fichier est introuvable
+        }
+
+        sf::Sprite bird;
+        bird.setTexture(birdTexture);
+        bird.setPosition(200.f, 540.f); // Position initiale
+
+public:
+    float birdVelocity = 0.f; // Vitesse verticale de l'oiseau
+    float gravity = 0.5f;     // Force gravitationnelle
+    float jumpStrength = -10.f; // Force d'un saut
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+        birdVelocity = jumpStrength; // Saut
+    }
+
+    birdVelocity += gravity; // Appliquer la gravité
+    bird.move(0.f, birdVelocity); // Déplacer l'oiseau
+
+};
 
 
 
@@ -14,16 +39,8 @@ using namespace std;
 
 
 
-/*sf::Texture birdTexture;
-if (!birdTexture.loadFromFile("flbird2.png")) {
-    return -1; // Erreur si le fichier est introuvable
-}
 
-sf::Sprite bird;
-bird.setTexture(birdTexture);
-bird.setPosition(200.f, 540.f); // Position initiale
-
-sf::Texture uppipe;
+/*sf::Texture uppipe;
 if (!uppipe.loadFromFile("pi.png")) {
     return -1; // Erreur si le fichier est introuvable
 }
@@ -37,10 +54,10 @@ if (!uppipe.loadFromFile("pip.png")) {
     return -1; // Erreur si le fichier est introuvable
 }
 
+
+
 RectangleShape pipe2(Vector2f(324, 624));
 pipe2.setTexture(&downpipe);
 pipe2.setPosition(500, 900);*/
-
-
 
 
