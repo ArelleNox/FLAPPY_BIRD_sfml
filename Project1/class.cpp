3c79b7a07651b2b8066ff7b9b89a15bd069b48a0
ArelleNox <janelleroxane@gmail.com>
2024-11-26 17:42:53 +0100

    ctor

--- Project1/class.cpp
+++ Project1/class.cpp
@@ -6,9 +6,16 @@
 using namespace sf;
 using namespace std;
 
+
+
+
 class Bird {
 private:
+    float birdVelocity = .0f;
+    float gravity = .5f;
+    float jumpStrength = -10.0f;
     
+    int texture() {
         sf::Texture birdTexture;
         if (!birdTexture.loadFromFile("flbird2.png")) {
             return -1; // Erreur si le fichier est introuvable
@@ -17,18 +24,23 @@ private:
         sf::Sprite bird;
         bird.setTexture(birdTexture);
         bird.setPosition(200.f, 540.f); // Position initiale
+    }
+   
 
 public:
-    float birdVelocity = 0.f; // Vitesse verticale de l'oiseau
-    float gravity = 0.5f;     // Force gravitationnelle
-    float jumpStrength = -10.f; // Force d'un saut
+    
+    Bird();
+    ~Bird();
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-        birdVelocity = jumpStrength; // Saut
+    void jump() {
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+            birdVelocity = jumpStrength; // Saut
+        }
+
+        birdVelocity += gravity; // Appliquer la gravité
+        bird.move(0.f, birdVelocity); // Déplacer l'oiseau
     }
-
-    birdVelocity += gravity; // Appliquer la gravité
-    bird.move(0.f, birdVelocity); // Déplacer l'oiseau
 
 };
 
